1624041ab8afe068ccfcbec7408060640a9e5fa8
dinhsan2000 <dinhsan200@gmail.com>
2020-08-30 09:59:03 +0700

    Rename Aosip to CherishOS

--- cherish_whyred.mk
+++ cherish_whyred.mk
@@ -19,16 +19,26 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common AOSiP stuff
-$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
+# Inherit some common Cherish stuff
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
 
 # Inherit from whyred device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
+#TARGET_GAPPS_ARCH := arm64
+TARGET_BOOT_ANIMATION_RES := 1080
+export CHERISH_BUILD_TYPE:= OFFICIAL
+$(call inherit-product, vendor/xiaomi/MiuiCamera/config.mk)
+# Vanilla
+CHERISH_NOGAPPS=true
+
+# Maintainer
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.cherish.maintainer=DinhSan
 
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := whyred
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := aosip_whyred
+PRODUCT_NAME := cherish_whyred
 PRODUCT_MODEL := Redmi Note 5
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
